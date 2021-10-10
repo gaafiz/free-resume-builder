698c4359901698a1a1da360b5348ab062e242a08
giulianfazio <giulian.fazio@gmail.com>
2021-10-10 11:49:24 +0200

    first component init

---
+++ src/assets/jsonResumeSample.ts
@@ -0,0 +1,103 @@
+{
+  "basics": {
+    "name": "",
+    "label": "", // I.e. Role
+    "image": "", // Link or Encoded picutre
+    "email": "",
+    "phone": "",
+    "url": "",
+    "summary": "",
+    "location": {
+      "address": "",
+      "postalCode": "",
+      "city": "",
+      "region": ""
+      "countryCode": "",
+    },
+    "profiles": [
+      {
+        "network": "",
+        "username": "",
+        "url": ""
+      }
+    ]
+  },
+  "work": [
+    {
+      "company": "",
+      "address": "",
+      "url": "",
+      "position": "",
+      "startDate": "2020-04",
+      "endDate": "Present",
+      "summary": "",
+    }
+  ],
+  "volunteer": [
+    {
+      "organization": "",
+      "position": "",
+      "url": "",
+      "startDate": "",
+      "endDate": "",
+      "summary": "",
+    }
+  ],
+  "education": [
+    {
+      "institution": "",
+      "address": ""
+      "url": ""
+      "area": "",
+      "studyType": "",
+      "startDate": "",
+      "endDate": "",
+      "score": "",
+      "summary": "",
+      "courses": [""] // Not used
+    }
+  ],
+  "awards": [
+    {
+      "title": "",
+      "date": "",
+      "awarder": "",
+      "summary": ""
+    }
+  ],
+  "skills": [
+    {
+      "name": "",
+      "level": "", // Not used
+      "keywords": [""]
+    }
+  ],
+  "languages": [
+    {
+      "languageCode": ""
+      "language": "",
+      "fluency": ""
+    }
+  ],
+  "interests": [
+    {
+      "name": "",
+      "keywords": [""]
+    }
+  ],
+  "references": [
+    {
+      "name": "",
+      "reference": "",
+    }
+  ],
+  "certificates": [
+    {
+      "title": "",
+      "issuer": "",
+      "date": "",
+      "url": "",
+      "summary": ""
+    }
+  ]
+};
