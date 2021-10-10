9104d3971e5daf10847f3927a62b59c93a68e4dc
giulianfazio <giulian.fazio@gmail.com>
2021-10-10 16:27:16 +0200

    added loading of jsonResumeFile

--- src/app/edit-content-tab/edit-content-tab.component.ts
+++ src/app/edit-content-tab/edit-content-tab.component.ts
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-content-tab.component.scss']
 })
 export class EditContentTabComponent implements OnInit {
+  userJsonResume: JSON;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onFileChanged(event) {
+    const files = event.target.files
+    if (files.length >= 1 ) {
+      const selectedFile = event.target.files[0];
+      const fileReader = new FileReader();
+      fileReader.readAsText(selectedFile, "UTF-8");
+      fileReader.onload = () => {
+        const fileAsText = fileReader.result as string;
+        this.userJsonResume = JSON.parse(fileAsText);
+        console.log(this.userJsonResume);
+      }
+      fileReader.onerror = (error) => {
+        console.log(error);
+      }
+    }
   }
 
 }
